d7a4801fad91726611ad586eda4af5e008f568bd
JonathanKross <kross.jonathan+github@gmail.com>
2016-04-19 23:02:43 -0400

    Updating sep.py

--- textminer/separator.py
+++ textminer/separator.py
@@ -1,14 +1,1 @@
 import re
-
-
-# @params("input,expected", [
-#     ("hello", ['hello']),
-#     ("hello world", ['hello', 'world']),
-#     ("raggggg hammer dog", ['raggggg', 'hammer', 'dog']),
-#     ("18-wheeler tarbox", ['18-wheeler', 'tarbox']),
-#     ("12", None),
-# ])
-def words(text):
-    return re.findall(r'', text)
-
-    assert s.words(input) == expected
